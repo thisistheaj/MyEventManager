f1e7fb01e0becd248e0acd9d9e4de17cb9e71e73
thisistheaj <ajbeckner>
2017-05-16 17:46:32 -0400

    wrote event detail page

--- src/pages/event-detail/event-detail.ts
+++ src/pages/event-detail/event-detail.ts
@@ -1,26 +1,25 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {EventProvider} from '../../providers/event/event';
 
-/**
- * Generated class for the EventDetailPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage({
-  name:'event-detail'
+  name: 'event-detail',
+  segment: 'event-detail/:eventId'
 })
 @Component({
   selector: 'page-event-detail',
   templateUrl: 'event-detail.html',
 })
 export class EventDetailPage {
+  public currentEvent: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public eventProvider: EventProvider) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EventDetailPage');
+  ionViewDidEnter() {
+    this.eventProvider.getEventDetail(this.navParams.get('eventId')).then(eventSnap => {
+      this.currentEvent = eventSnap;
+    });
   }
 
 }
