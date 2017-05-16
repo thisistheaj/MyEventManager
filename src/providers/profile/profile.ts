199a53a99f8b5556826fdb9fc4b47869fa0268f9
thisistheaj <ajbeckner>
2017-05-16 15:41:56 -0400

    wrote profile provider

--- src/providers/profile/profile.ts
+++ src/providers/profile/profile.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+import * as firebase from "firebase/app";
 /*
   Generated class for the ProfileProvider provider.
 
@@ -13,6 +14,49 @@ export class ProfileProvider {
 
   constructor(public http: Http) {
     console.log('Hello ProfileProvider Provider');
+  }
+
+  getUserProfile(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      firebase.database().ref('/userProfile').child(firebase.auth().currentUser.uid).on('value',data => {
+        resolve(data.val());
+      });
+    });
+  }
+
+  updateName(firstName: string,lastName:string): firebase.Promise<any> {
+    return firebase.database().ref('/userProfile').child(firebase.auth().currentUser.uid).update({
+      firstName: firstName,
+      lastName: lastName
+    });
+  }
+
+  updateDOB(birthDate:string): firebase.Promise<any> {
+    return firebase.database().ref('/userProfile').child(firebase.auth().currentUser.uid).update({
+      birthDate:birthDate
+    });
+  }
+
+  updateEmail(newEmail:string, password: string): firebase.Promise<any>{
+    const credential = firebase.auth.EmailAuthProvider.credential(firebase.auth().currentUser.email,password);
+    return firebase.auth().currentUser.reauthenticate(credential).then(user =>{
+      firebase.auth().currentUser.updateEmail(newEmail).then( user => {
+        firebase.database().ref('/userProfile').child(firebase.auth().currentUser.uid).update({
+          email: newEmail
+        });
+      });
+    });
+  }
+
+  updatePassword(newPassword:string,password:string): firebase.Promise<any>{
+    const credential = firebase.auth.EmailAuthProvider.credential(firebase.auth().currentUser.email,password);
+    return firebase.auth().currentUser.reauthenticate(credential).then(user => {
+      firebase.auth().currentUser.updatePassword(newPassword).then(user => {
+        console.log('Password Changed');
+      }, err => {
+        console.log(err);
+      })
+    })
   }
 
 }
